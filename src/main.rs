7956eaec5c08c4268fa6e98e36ce393fbed93005
unbellum <unbellum@yahoo.com>
2022-12-18 21:08:48 -0500

    The first step, plan out what I want to do

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// TODO:
+//
+// 1. Want a separation between a game project and the engine itself
+// 2. Setup a workflow for the client project to funnel data into the engine (replacement for the application class we would use in C++)
+// 3. Setup a window with the rust equivalent to ImGui
+// 4. Add a game loop
+// 5. Setup a OpenGL rendering viewport
+// 6. Setup input handlers
+
+fn main() {
+    println!("Hello, world!");
+}
